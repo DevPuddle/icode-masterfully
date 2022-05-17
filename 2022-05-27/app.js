ec0aa3b1949652f1d6f25702a2b8f6e6c8f6da1c
Isaiah Henson <Devpuddles@gmail.com>
2022-05-17 19:36:06 -0400

    cc may 17

---
+++ 2022-05-27/app.js
@@ -0,0 +1,18 @@
+//PREP
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//P: take boolean value, do you pass in a number to compare true or false? yes
+// A boolean value is either a true or false
+//R: return "Yes" if boolean is true "No" if false
+// Both sides of if else need a return value
+//E:
+function boolToWord( bool ){
+    if(bool === true) {
+        return "Yes"
+    }else 
+        return "No"
+  }
+
+//boolToWord(2)//true if approved by conditional
+//P: if(bool === false){
+//return "Yes" Else "No"
